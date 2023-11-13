2b9a9ac510fe43a85171a46c23258887e7ea7341
Mazztok45 <maxence@dogppol.zblmath.fiz-karlsruhe.de>
2023-11-13 17:06:42 +0100

    function to get all attributes, need improvements

--- src/zbmath_rest2oai/getWithSwagger.py
+++ src/zbmath_rest2oai/getWithSwagger.py
@@ -42,5 +42,30 @@ def append_text_child(xmld, parent, name, value):
 
 
 final_xml = append_text_child(xmld, ron, "id", res.result.id)
+#print(final_xml)
 
-print(final_xml.toprettyxml())
+def func_json_keys(json_part, l):
+    
+    try:
+        print('test0')
+        json_part.__dict__
+            #if type(json_part)==dict:
+        index = json_part.__dict__.keys()
+    except:
+        print('test1')
+        index = json_part
+        for elem in index:
+            if type(json_part[elem].__dict__)==dict or type(json_part[elem])==list:
+                func_json_keys(json_part[elem], l)
+            else:
+                l.append([elem, json_part[elem]])
+
+    return l
+
+l0=[]
+infos_list = func_json_keys(dict(res.__dict__), l0)
+print(infos_list)
+for j in infos_list:
+    final_xml = append_text_child(xmld, final_xml, j[0], j[1])
+
+#print(final_xml.toprettyxml())
