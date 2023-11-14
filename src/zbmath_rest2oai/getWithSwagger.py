ca49c75fd8ecba2b42030f0e1f75d7abe0e6dce0
Mazztok45 <maxence.azzouz@gmail.com>
2023-11-14 17:31:36 +0100

    improvement of the function func_json_keys to better fetch data in depth. The final xml is now much enriched.

--- src/zbmath_rest2oai/getWithSwagger.py
+++ src/zbmath_rest2oai/getWithSwagger.py
@@ -7,6 +7,7 @@ res = api_instance.get_document_by_zbmath_id_document_id_get(id="6383667")
 doc = res.result
 xmld = create_document(doc)
 
+#print(res.result)
 
 ron = xmld.createElement("oai_zb_preview:zbmath")
 ron.setAttributeNS(
@@ -45,27 +46,59 @@ final_xml = append_text_child(xmld, ron, "id", res.result.id)
 #print(final_xml)
 
 def func_json_keys(json_part, l):
-    
-    try:
-        print('test0')
-        json_part.__dict__
-            #if type(json_part)==dict:
-        index = json_part.__dict__.keys()
-    except:
-        print('test1')
-        index = json_part
-        for elem in index:
-            if type(json_part[elem].__dict__)==dict or type(json_part[elem])==list:
-                func_json_keys(json_part[elem], l)
-            else:
-                l.append([elem, json_part[elem]])
 
-    return l
+    if json_part is None:
+        print(json_part)
+        return l
+
+    elif str(json_part).startswith('{'):
+        try:
+            print('test_dict')
+            index = json_part.__dict__.keys()
+            json_part = json_part.__dict__
+        except:
+            print('test1')
+            index = json_part
+
+        if type(json_part) == dict:
+            for elem in index:
+                #print('call func')
+                if type(json_part[elem]) in [str, int, float, None]:
+                    l.append([str(elem), json_part[elem]])
+                #elif json_part[elem] == list():
+                    #continue
+
+                elif str(json_part[elem]).startswith('{'):
+                    func_json_keys(json_part[elem], l)
+                else:
+                    print('UNKNOWN TYPE')
+                    print(type(json_part[elem]))
+                    print(json_part[elem])
+                    func_json_keys(json_part[elem], l)
+            return l
+
+    elif str(json_part).startswith('['):
+        try:
+            print('test_list')
+            #json_part = str(json_part).split(',')
+        except:
+            print('test_list_except')
+
+        for i in range(len(json_part)):
+            # print('call func')
+            if type(json_part[i]) in [str, int, float, None]:
+                l.append(json_part[i])
+            elif str(json_part[i]).startswith('{'):
+                func_json_keys(json_part[i], l)
+        return l
+
+    else:
+        return l
 
 l0=[]
-infos_list = func_json_keys(dict(res.__dict__), l0)
-print(infos_list)
+infos_list = func_json_keys(res, l0)
+#print(infos_list)
 for j in infos_list:
     final_xml = append_text_child(xmld, final_xml, j[0], j[1])
 
-#print(final_xml.toprettyxml())
+print(final_xml.toprettyxml())
