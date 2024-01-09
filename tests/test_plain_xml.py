2abeae7e1eca4a52a3e4379c1648ce442ba3b473
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-01-09 20:24:06 +0100

    Add plain XML test

---
+++ tests/test_plain_xml.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+from xml.dom.minidom import parse
+from xmldiff import main, formatting
+from xmldiff.actions import MoveNode
+from zbmath_rest2oai import getAsXml
+
+
+class PlainXmlTest(unittest.TestCase):
+    def test_similarity(self):
+        real_string = getAsXml.final_xml2("6383667")
+        ref_location = os.path.join(os.path.dirname(__file__), './data/plain.xml')
+        with open(ref_location) as f:
+            dom = parse(f)
+            expected_string = dom.toprettyxml()
+            diff = main.diff_texts(expected_string, real_string, {
+                'ratio_mode': 'accurate'
+            })
+            essentials = list(filter(lambda e: not isinstance(e, MoveNode), diff))
+            self.assertLessEqual(len(essentials), 333)
+            diff_text = main.diff_texts(expected_string, real_string, {
+                'ratio_mode': 'accurate'
+            }, formatter=formatting.XMLFormatter())
+            print(diff_text)
+
+
+if __name__ == '__main__':
+    unittest.main()
