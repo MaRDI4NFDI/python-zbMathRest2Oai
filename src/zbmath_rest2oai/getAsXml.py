4018e5aee40146cc2d9d06eb81f60099737bfd4f
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-01-09 23:46:16 +0100

    Handle special case for states

--- src/zbmath_rest2oai/getAsXml.py
+++ src/zbmath_rest2oai/getAsXml.py
@@ -7,8 +7,15 @@ def final_xml2(de):
     headers = {'Accept': 'application/json'}
 
     r = requests.get('https://api.zbmath.org/v1/document/' + de, headers=headers)
-
-    return dict2xml.Converter(wrap="root").build(r.json(), closed_tags_for=[[], '', [None], None])
-
-
-print(final_xml2("6383667"))
+    json = r.json()
+    states = {}
+    for lst in json['result']['states']:
+        [k, v] = lst
+        states[k] = v
+    json['result']['states'] = states
+    return (
+        dict2xml.Converter(wrap="root")
+        .build(r.json(), closed_tags_for=[
+            [], '', [None], None
+        ])
+    )
