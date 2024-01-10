3b61ca4182cec2e5175b294a1076a920cfebe84a
Moritz Schubotz <github@ckurs.de>
2024-01-10 14:18:41 +0100

    Update getAsXml.py

--- src/zbmath_rest2oai/getAsXml.py
+++ src/zbmath_rest2oai/getAsXml.py
@@ -8,14 +8,17 @@ def final_xml2(de):
 
     r = requests.get('https://api.zbmath.org/v1/document/' + de, headers=headers)
     json = r.json()
+    # Bugfix as the sates are lists of lists which has no canonical XML mapping
     states = {}
     for lst in json['result']['states']:
         [k, v] = lst
         states[k] = v
     json['result']['states'] = states
+    # End of fix
     return (
         dict2xml.Converter(wrap="root")
         .build(json, closed_tags_for=[
+            # Bugfix for wired XML output such as the string None or </>
             [], '', [None], None
         ])
     )
