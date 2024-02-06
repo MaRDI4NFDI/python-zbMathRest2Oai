02cf02f3b22fe0e1f277846538715123c2d09db5
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-02-06 23:00:16 +0100

    Fix auth problems in xslt script

--- xslt/xslt.sh
+++ xslt/xslt.sh
@@ -1,3 +1,4 @@
-AUTH=$(echo -ne "$OAI_BASIC_USER:$OAI_BASIC_PASSWORD" | base64 --wrap 0)
+#!/bin/bash
+AUTH=$(echo -n "$OAI_BASIC_USER:$OAI_BASIC_PASSWORD" | base64)
 XSLT_RADAR_DC='https://github.com/MaRDI4NFDI/python-zbMathRest2Oai/blob/main/xslt/xslt-article-transformation.xslt'
-curl --noproxy '*' -X POST -H 'Content-Type: application/json' -i 'http://oai-input.portal.mardi4nfdi.de/oai-backend/item' --header "Authorization: Basic $AUTH" --data '{"name":"Radar2OAI_DC_v09","formatFrom":"radar","formatTo":"oai_dc","xsltStylesheet":'"$XSLT_RADAR_DC}"'}'
+curl --noproxy '*' -X POST -H 'Content-Type: application/json' -i 'https://oai-input.portal.mardi4nfdi.de/oai-backend/item' --header "Authorization: Basic $AUTH" --data '{"name":"Radar2OAI_DC_v09","formatFrom":"radar","formatTo":"oai_dc","xsltStylesheet":'"$XSLT_RADAR_DC}"'}'
