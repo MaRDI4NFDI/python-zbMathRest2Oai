1c824dcb3f16bf5c2084b2232f842adc2116bbb1
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-07 18:04:09 +0100

    using the new zbmath_rest_api format to create a new crosswalk

--- xslt/xslt.sh
+++ xslt/xslt.sh
@@ -3,4 +3,4 @@ AUTH=$(echo -n "$OAI_BASIC_USER:$OAI_BASIC_PASSWORD" | base64)
 XSLT_RADAR_DATACITE='cat xslt-article-transformation.xslt | jq -Rsa . '
 VAR_XSLT_RADAR_DATACITE=$(eval "$XSLT_RADAR_DATACITE")
 
-curl --noproxy '*' -X POST -H 'Content-Type: application/json' -i 'https://oai-input.portal.mardi4nfdi.de/oai-backend/crosswalk' --header "Authorization: Basic $AUTH" --data '{"name":"Radar2OAI_DATACITE","formatFrom":"radar","formatTo":"datacite","xsltStylesheet":'"$VAR_XSLT_RADAR_DATACITE}"'}'
+curl --noproxy '*' -X POST -H 'Content-Type: application/json' -i 'https://oai-input.portal.mardi4nfdi.de/oai-backend/crosswalk' --header "Authorization: Basic $AUTH" --data '{"name":"Radar2OAI_DATACITE3","formatFrom":"zbmath_rest_api","formatTo":"datacite","xsltStylesheet":'"$VAR_XSLT_RADAR_DATACITE}"'}'
