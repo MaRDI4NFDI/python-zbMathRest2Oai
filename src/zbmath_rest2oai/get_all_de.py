6580eb4c544e36e9e758d1cb42936ea36a968d85
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-02-07 22:33:26 +0100

    Fix PEP

--- src/zbmath_rest2oai/get_all_de.py
+++ src/zbmath_rest2oai/get_all_de.py
@@ -9,7 +9,6 @@ from zbmath_rest2oai.writeOai import write_oai
 CSV_URL = 'https://github.com/MaRDI4NFDI/python-zbMathRest2Oai/releases/download/test/all_de_240115.csv'
 
 
-
 def get_all_de():
     with requests.get(CSV_URL, stream=True) as r:
         lines = (line.decode('utf-8') for line in r.iter_lines())
