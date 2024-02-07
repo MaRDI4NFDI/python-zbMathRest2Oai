e1273d3a5625f42721203551efdc322ebd96586a
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-07 11:53:12 +0100

    changing folder from format to crosswalk in xslt.sh + fixing error in string reading the shell code + xslt now available on https://oai-input.portal.mardi4nfdi.de/oai-backend/crosswalk

--- xslt/xslt-article-transformation.xslt
+++ xslt/xslt-article-transformation.xslt
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="ISO-8859-1"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:oai_zb_preview="https://zbmath.org/OAI/2.0/oai_zb_preview/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:zbmath="https://zbmath.org/zbmath/elements/1.0/">
 <xsl:template match="/">
 <oai_zb_preview>
--- xslt/xslt.sh
+++ xslt/xslt.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 AUTH=$(echo -n "$OAI_BASIC_USER:$OAI_BASIC_PASSWORD" | base64)
-XSLT_RADAR_DC='https://github.com/MaRDI4NFDI/python-zbMathRest2Oai/blob/main/xslt/xslt-article-transformation.xslt'
-curl --noproxy '*' -X POST -H 'Content-Type: application/json' -i 'https://oai-input.portal.mardi4nfdi.de/oai-backend/item' --header "Authorization: Basic $AUTH" --data '{"name":"Radar2OAI_DC_v09","formatFrom":"radar","formatTo":"oai_dc","xsltStylesheet":'"$XSLT_RADAR_DC}"'}'
+XSLT_RADAR_DATACITE='cat xslt-article-transformation.xslt | jq -Rsa . '
+VAR_XSLT_RADAR_DATACITE=$(eval "$XSLT_RADAR_DATACITE")
+
+curl --noproxy '*' -X POST -H 'Content-Type: application/json' -i 'https://oai-input.portal.mardi4nfdi.de/oai-backend/crosswalk' --header "Authorization: Basic $AUTH" --data '{"name":"Radar2OAI_DATACITE","formatFrom":"radar","formatTo":"datacite","xsltStylesheet":'"$VAR_XSLT_RADAR_DATACITE}"'}'
