084adff87c9307e78cfd45169c2c24771c894fbe
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-08 18:43:40 +0100

    CSV added as parameter in run_get_all_documents_py

--- src/zbmath_rest2oai/get_all_de.py
+++ src/zbmath_rest2oai/get_all_de.py
@@ -6,10 +6,9 @@ import csv
 from zbmath_rest2oai.writeOai import write_oai
 
 # from https://stackoverflow.com/a/38677619/9215209
-CSV_URL = 'https://github.com/MaRDI4NFDI/python-zbMathRest2Oai/releases/download/test/all_de_240115.csv'
 
 
-def get_all_de(api_source):
+def get_all_de(api_source,CSV_URL):
     with requests.get(CSV_URL, stream=True) as r:
         lines = (line.decode('utf-8') for line in r.iter_lines())
         for row in csv.reader(lines):
