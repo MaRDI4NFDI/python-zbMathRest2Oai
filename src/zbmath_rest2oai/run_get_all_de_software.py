60485e7e034284221a7e0f65d35905fe56c07fea
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-08 17:42:40 +0100

    adding api path into test_plain_xml.py to make final_xml2 function working again

--- src/zbmath_rest2oai/run_get_all_de_software.py
+++ src/zbmath_rest2oai/run_get_all_de_software.py
@@ -2,4 +2,4 @@ from zbmath_rest2oai.get_all_de import get_all_de
 
 
 if __name__ == '__main__':
-    get_all_de('https://api.zbmath.org/v1/document/')
+    get_all_de('https://api.zbmath.org/v1/software/')
