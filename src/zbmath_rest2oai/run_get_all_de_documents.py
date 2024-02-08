3bee580c4e6ba515e2de59e4fc0631d5cd0c2d10
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-08 18:40:46 +0100

    adding CSV_URL

--- src/zbmath_rest2oai/run_get_all_de_documents.py
+++ src/zbmath_rest2oai/run_get_all_de_documents.py
@@ -1,5 +1,6 @@
 from zbmath_rest2oai.get_all_de import get_all_de
 
+CSV_URL = 'https://github.com/MaRDI4NFDI/python-zbMathRest2Oai/releases/download/test/all_de_240115.csv'
 
 if __name__ == '__main__':
     get_all_de('https://api.zbmath.org/v1/document/')
