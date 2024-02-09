aeb38579e064881520c08eeb0c8b8b4a2d669253
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-09 11:53:54 +0100

    modify software.csv

--- src/zbmath_rest2oai/fake_dict_sw.py
+++ src/zbmath_rest2oai/fake_dict_sw.py
@@ -1,6 +1,6 @@
 def fake_dict_sw():
     with open('software.csv','w') as f:
-        return f.write(','.join(map(str,list(range(1, 10)))))
+        return f.write("\n".join(map(str,list(range(2, 10)))))
 
 if __name__ == '__main__':
     fake_dict_sw()
--- src/zbmath_rest2oai/run_get_all_de_documents.py
+++ src/zbmath_rest2oai/run_get_all_de_documents.py
@@ -1,6 +1,6 @@
 from zbmath_rest2oai.get_all_de import get_all_de
 
-CSV_URL = 'https://github.com/MaRDI4NFDI/python-zbMathRest2Oai/releases/download/test/all_de_240115.csv'
+CSV_URL = 'https://github.com/MaRDI4NFDI/python-zbMathRest2Oai/blob/75d00a4dcd97684ae03dce7059c5c3d260bdf47c/src/zbmath_rest2oai/software.csv'
 
 if __name__ == '__main__':
     get_all_de('https://api.zbmath.org/v1/document/',CSV_URL)
