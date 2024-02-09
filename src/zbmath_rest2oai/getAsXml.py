0a3d708a3237b4e11ad30c66b9dac8fbd9c27476
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-09 12:58:37 +0100

    added a condition json[result] is a dictionnary to make the distinction to avoid bug when we run run_get_all_de_software.py as json[result] is not a dictionnary when we request the software endpoint of the rest api

--- src/zbmath_rest2oai/getAsXml.py
+++ src/zbmath_rest2oai/getAsXml.py
@@ -10,11 +10,13 @@ def final_xml2(de, api_source):
         raise Exception(f"Unexpected response with status code {r.status_code}: {r.text}")
     json = r.json()
     # Bugfix as the sates are lists of lists which has no canonical XML mapping
-    states = {}
-    for lst in json['result']['states']:
-        [k, v] = lst
-        states[k] = v
-    json['result']['states'] = states
+    if type(json['result'])==dict:
+
+        states = {}
+        for lst in json['result']['states']:
+            [k, v] = lst
+            states[k] = v
+        json['result']['states'] = states
     # End of fix
     return (
         dict2xml.Converter(wrap="root")
