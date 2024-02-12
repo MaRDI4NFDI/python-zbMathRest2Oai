751563bbe3cb32b42723f3cb5bd96cf9f13f245d
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-12 18:32:18 +0100

    adapting xslt-software-datacite.sh variable names to the xslt file for software

--- xslt/software/xslt-software-datacite.sh
+++ xslt/software/xslt-software-datacite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 AUTH=$(echo -n "$OAI_BASIC_USER:$OAI_BASIC_PASSWORD" | base64)
-XSLT_RADAR_DATACITE='cat xslt-article-transformation.xslt | jq -Rsa . '
-VAR_XSLT_RADAR_DATACITE=$(eval "$XSLT_RADAR_DATACITE")
+XSLT_ZBMATH_REST_API_DATACITE='cat xslt-software-transformation.xslt | jq -Rsa . '
+VAR_XSLT_ZBMATH_REST_API_DATACITE=$(eval "$XSLT_ZBMATH_REST_API_DATACITE")
 
-curl --noproxy '*' -X POST -H 'Content-Type: application/json' -i 'https://oai-input.portal.mardi4nfdi.de/oai-backend/crosswalk' --header "Authorization: Basic $AUTH" --data '{"name":"rest2preview","formatFrom":"zbmath_rest_api","formatTo":"oai_zb_preview","xsltStylesheet":'"$VAR_XSLT_RADAR_DATACITE}"'}'
+curl --noproxy '*' -X POST -H 'Content-Type: application/json' -i 'https://oai-input.portal.mardi4nfdi.de/oai-backend/crosswalk' --header "Authorization: Basic $AUTH" --data '{"name":"rest2preview","formatFrom":"zbmath_rest_api","formatTo":datacite","xsltStylesheet":'"$VAR_XSLT_ZBMATH_REST_API_DATACITE}"'}'
