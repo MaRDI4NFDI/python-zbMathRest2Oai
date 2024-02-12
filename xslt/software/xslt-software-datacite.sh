a2498a3d3abb6c70d3c143bdb1d017edc0a2c798
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-12 18:46:01 +0100

    fixing quote issue in xslt-software-datacite.sh

--- xslt/software/xslt-software-datacite.sh
+++ xslt/software/xslt-software-datacite.sh
@@ -3,4 +3,4 @@ AUTH=$(echo -n "$OAI_BASIC_USER:$OAI_BASIC_PASSWORD" | base64)
 XSLT_ZBMATH_REST_API_DATACITE='cat xslt-software-transformation.xslt | jq -Rsa . '
 VAR_XSLT_ZBMATH_REST_API_DATACITE=$(eval "$XSLT_ZBMATH_REST_API_DATACITE")
 
-curl --noproxy '*' -X POST -H 'Content-Type: application/json' -i 'https://oai-input.portal.mardi4nfdi.de/oai-backend/crosswalk' --header "Authorization: Basic $AUTH" --data '{"name":"rest2preview","formatFrom":"zbmath_rest_api","formatTo":datacite","xsltStylesheet":'"$VAR_XSLT_ZBMATH_REST_API_DATACITE}"'}'
+curl --noproxy '*' -X POST -H 'Content-Type: application/json' -i 'https://oai-input.portal.mardi4nfdi.de/oai-backend/crosswalk' --header "Authorization: Basic $AUTH" --data '{"name":"software_restapi_to_datacite","formatFrom":"zbmath_rest_api","formatTo":"datacite","xsltStylesheet":'"$VAR_XSLT_ZBMATH_REST_API_DATACITE}"'}'
