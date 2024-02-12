5b803dadfdf049665aed24ec2188a67fdd897a03
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-12 14:21:46 +0100

    moving reference.xml to articles folder

--- test/test_oai_preview.py
+++ test/test_oai_preview.py
@@ -11,7 +11,7 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         real = getWithSwagger.get_final_xml("6383667")
         real_string = real.parentNode.parentNode.toprettyxml()
-        ref_location = os.path.join(os.path.dirname(__file__), './data/reference.xml')
+        ref_location = os.path.join(os.path.dirname(__file__), 'data/articles/reference.xml')
         with open(ref_location) as f:
             dom = parse(f)
             expected_string = dom.toprettyxml()
