297ab51d0b0bddd58d2e33c59ee5001d647a168c
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-02-16 12:47:33 +0100

    Fix ws issues

--- src/zbmath_rest2oai/get_all_de.py
+++ src/zbmath_rest2oai/get_all_de.py
@@ -8,13 +8,13 @@ from zbmath_rest2oai.writeOai import write_oai
 # from https://stackoverflow.com/a/38677619/9215209
 
 
-def get_all_de(api_source,CSV_URL):
+def get_all_de(api_source, CSV_URL):
     with requests.get(CSV_URL, stream=True) as r:
         lines = (line.decode('utf-8') for line in r.iter_lines())
         for row in csv.reader(lines):
             de = row[0]
             try:
-                write_oai(de,api_source)
+                write_oai(de, api_source)
             except Exception as error:
                 print(de, error, file=sys.stderr)
 
