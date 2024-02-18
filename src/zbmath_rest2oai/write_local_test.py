a5015221938efaf54c05dd0e5b815f856e021734
Azzouz-Thuderoz <maxence.azzouz@gmail,com>
2024-02-18 14:41:31 +0100

    write_local_test.py now updates local test files

--- src/zbmath_rest2oai/write_local_test.py
+++ src/zbmath_rest2oai/write_local_test.py
@@ -1,17 +1,7 @@
-import json
-
-import requests
-import os
-
-from requests.auth import HTTPBasicAuth
-
 from zbmath_rest2oai import getAsXml
 import sys
 
-def write_oai(x, api_source):
-    test_xml = getAsXml.final_xml2(x, api_source)
-    return
 
 if __name__ == '__main__':
-
-    print(write_oai(sys.argv[1], sys.argv[2]))
+    with open(sys.argv[3], 'w') as f:
+        f.write(getAsXml.final_xml2(sys.argv[1], sys.argv[2]))
