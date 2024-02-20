9aef821f1628e5cba8610eeae59f581ef73c0b56
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-20 18:42:39 +0100

    adjusting file paths

--- test/test_node_names_software_datacite.py
+++ test/test_node_names_software_datacite.py
@@ -1,7 +1,7 @@
 import lxml.etree as ET
 
-dom = ET.parse('')
-xslt = ET.parse(xsl_filename)
+dom = ET.parse('python-zbMathRest2Oai/test/data/software/plain.xml')
+xslt = ET.parse('python-zbMathRest2Oai/xslt/software/xslt-software-transformation.xslt')
 transform = ET.XSLT(xslt)
 newdom = transform(dom)
 print(ET.tostring(newdom, pretty_print=True)
