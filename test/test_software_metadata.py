00738376ef5a9f625e52af4144da353e42ba26c1
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-20 18:20:03 +0100

    adjusting regex for result from the API in the test file

--- test/test_software_metadata.py
+++ test/test_software_metadata.py
@@ -11,14 +11,16 @@ from zbmath_rest2oai import getAsXml
 class PlainXmlTest(unittest.TestCase):
     def test_similarity(self):
         real_string = getAsXml.final_xml2("2", 'https://api.zbmath.org/v1/software/_search?search_string=si%3A')
-        real_string = re.sub(
+        real_string  = re.sub(
             '<query_execution_time_in_seconds>0.\\d+</query_execution_time_in_seconds>',
-            '<query_execution_time_in_seconds>0</query_execution_time_in_seconds>',
+            '',
             real_string)
         real_string = re.sub(
             '<time_stamp>[\\d\\-: .]+</time_stamp>',
-            '<time_stamp>0</time_stamp>',
+            '',
             real_string)
+        xml_string = [line for line in xml_string.splitlines() if line.strip() != '']
+        xml_string = '\n'.join(xml_string)
 
         ref_location = os.path.join(os.path.dirname(__file__), 'data/software/plain.xml')
         with open(ref_location) as f:
