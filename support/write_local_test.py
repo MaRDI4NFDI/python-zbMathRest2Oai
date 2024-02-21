faba3c66c8cc6dd0c4cb0b90b76f947bad844529
Mazztok45 <maxence.azzouz@gmail.com>
2024-02-21 15:15:17 +0100

    replacing with 0 in plain.xml

--- support/write_local_test.py
+++ support/write_local_test.py
@@ -5,11 +5,11 @@ xml_string = getAsXml.final_xml2(sys.argv[1], sys.argv[2])
 
 xml_string = re.sub(
     '<query_execution_time_in_seconds>0.\\d+</query_execution_time_in_seconds>',
-    '',
+    '<query_execution_time_in_seconds>0</query_execution_time_in_seconds>',
     xml_string)
 xml_string = re.sub(
     '<time_stamp>[\\d\\-: .]+</time_stamp>',
-    '',
+    '<time_stamp>0</time_stamp>',
     xml_string)
 
 print(xml_string)
