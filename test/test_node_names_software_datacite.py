dd46a7a6cbcb85a068157155c06bff8131b873cc
Azzouz-Thuderoz <maxence.azzouz@gmail,com>
2024-02-22 20:01:18 +0100

    fixed encoding issues and & escapement issues, first prototype of tests executing, a lot of errors. will be improvement in future commits

--- test/test_node_names_software_datacite.py
+++ test/test_node_names_software_datacite.py
@@ -1,7 +1,34 @@
-import lxml.etree as ET
 
+import os
+import re
+import unittest
+
+import lxml.etree as ET
+from xmldiff import main
+from xmldiff.actions import MoveNode
+from xml.dom.minidom import parse
+from zbmath_rest2oai import getAsXml
 dom = ET.parse('test/data/software/plain.xml')
 xslt = ET.parse('xslt/software/xslt-software-transformation.xslt')
 transform = ET.XSLT(xslt)
 newdom = transform(dom)
-print(ET.tostring(newdom, pretty_print=True))
+real_string = str(newdom).replace('&','&amp;')
+print(real_string)
+class PlainXmlTest(unittest.TestCase):
+    def test_similarity(self):
+
+        ref_location = os.path.join(os.path.dirname(__file__), 'data/software/reference.xml')
+        with open(ref_location) as f:
+            dom = parse(f)
+            expected_string = dom.toprettyxml()
+            print(type(expected_string))
+            diff = main.diff_texts(expected_string, real_string, {
+                'ratio_mode': 'fast',
+                'F': 1,
+            })
+            essentials = list(filter(lambda e: not isinstance(e, MoveNode), diff))
+            self.assertLessEqual(len(essentials), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
