8d55593d1b3bc2caaa4b103fa8f9f5cc2c36afcc
Mazztok45 <maxence.azzouz@gmail.com>
2024-03-06 17:17:31 +0100

    adding new API link

--- src/zbmath_rest2oai/run_get_all_de_software.py
+++ src/zbmath_rest2oai/run_get_all_de_software.py
@@ -1,5 +1,7 @@
 from zbmath_rest2oai.get_all_de import get_all_de
 
-CSV_URL = 'https://raw.githubusercontent.com/MaRDI4NFDI/python-zbMathRest2Oai/main/src/zbmath_rest2oai/software.csv'
+#CSV_URL = 'https://raw.githubusercontent.com/MaRDI4NFDI/python-zbMathRest2Oai/main/src/zbmath_rest2oai/software.csv'
 if __name__ == '__main__':
-    get_all_de('https://api.zbmath.org/v1/software/_search?search_string=si%3A',CSV_URL, 'oai:swmath.org:')
+    #get_all_de('https://api.zbmath.org/v1/software/_search?search_string=si%3A',CSV_URL, 'oai:swmath.org:')
+    get_all_de('https://api.zbmath.org/v1/software/_search?search_string=si%3A0-100&page=0&results_per_page=100', 'oai:swmath.org:')
+    get_all_de('https://api.zbmath.org/v1/software/_search?search_string=si%3A100-200&page=0&results_per_page=100', 'oai:swmath.org:')
