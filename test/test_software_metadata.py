5e0372a180d55bb46b1e7a4aa1310701ffb30dff
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-03-11 20:57:54 +0100

    Remove status section from software test

--- test/test_software_metadata.py
+++ test/test_software_metadata.py
@@ -13,14 +13,6 @@ API_SOURCE = 'https://api.zbmath.org/v1/software/_search?search_string=si%3A2'
 class PlainXmlTest(unittest.TestCase):
     def test_similarity(self):
         real_string = getAsXml.final_xml2(API_SOURCE)[2]
-        real_string = re.sub(
-            '<query_execution_time_in_seconds>0.\\d+</query_execution_time_in_seconds>',
-            '<query_execution_time_in_seconds>0</query_execution_time_in_seconds>',
-            real_string)
-        real_string = re.sub(
-            '<time_stamp>[\\d\\-: .]+</time_stamp>',
-            '<time_stamp>0</time_stamp>',
-            real_string)
         real_string = [line for line in real_string.splitlines() if line.strip() != '']
         real_string = '\n'.join(real_string)
 
