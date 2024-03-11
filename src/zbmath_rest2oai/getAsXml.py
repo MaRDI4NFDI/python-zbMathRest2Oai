db1a03d488d34420d91411c6171154279467380a
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2024-03-11 20:49:51 +0100

    Style: Improve readability of final_xml2

--- src/zbmath_rest2oai/getAsXml.py
+++ src/zbmath_rest2oai/getAsXml.py
@@ -1,23 +1,21 @@
-import dict2xml
+from dict2xml import Converter
 import requests
 import sys
 
-def final_xml2(api_source):
 
+def final_xml2(api_source):
     headers = {'Accept': 'application/json'}
     r = requests.get(api_source, headers=headers)
     if r.status_code != 200:
         raise Exception(f"Unexpected response with status code {r.status_code}: {r.text}")
     json = r.json()
-    dict_math_entities= dict()
-    for i in range(len(json["result"])):
-        dict_math_entities[json["result"][i]["id"]]=dict2xml.Converter(wrap="root").build(json["result"][i], closed_tags_for=[[], '', [None], None])
-    #print(dict_math_entities)
-    # Bugfix as the sates are lists of lists which has no canonical XML mapping`
-    # End of fix
+    dict_math_entities = dict()
+    for result in json["result"]:
+        dict_math_entities[result["id"]] = Converter(wrap="root").build(
+            result,
+            closed_tags_for=[[], '', [None], None])
     return dict_math_entities
-    
+
 
 if __name__ == "__main__":
     final_xml2(sys.argv[1])
-
