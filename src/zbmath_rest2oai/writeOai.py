f3804b30cf524375cf195b2fc5085a880188e6fa
Mazztok45 <maxence.azzouz@gmail.com>
2024-03-11 14:13:52 +0100

    fixing parameter error message

--- src/zbmath_rest2oai/writeOai.py
+++ src/zbmath_rest2oai/writeOai.py
@@ -11,7 +11,7 @@ from zbmath_rest2oai import getAsXml
 def write_oai(api_source, prefix):
     test_xml = getAsXml.final_xml2(api_source)
     for identifier in test_xml.keys():
-        files = {"item": (None, json.dumps({"identifier": prefix+identifier,"deleteFlag": False,"ingestFormat": "zbmath_rest_api",}),"application/json",),"content": (None, test_xml[key]),}
+        files = {"item": (None, json.dumps({"identifier": prefix+str(identifier),"deleteFlag": False,"ingestFormat": "zbmath_rest_api",}),"application/json",),"content": (None, test_xml[identifier]),}
         basic = HTTPBasicAuth('swmath', os.environ.get('OAI_BASIC_PASSWORD'))
         x = requests.post("http://oai-input.portal.mardi4nfdi.de/oai-backend/item", files=files, auth=basic)
         if x.status_code != 200:
@@ -22,5 +22,4 @@ def write_oai(api_source, prefix):
 
 if __name__ == '__main__':
     import sys
-
     write_oai(sys.argv[1], sys.argv[2])
