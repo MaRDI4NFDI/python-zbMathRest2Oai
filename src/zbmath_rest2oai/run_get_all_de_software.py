d9330e888c67da734c4fae90e185b2a386d6b317
Mazztok45 <maxence.azzouz@gmail.com>
2024-03-14 12:35:52 +0100

    modifying run_get_all_de_software.py to run all software metadata writing to the OAI-PMH server

--- src/zbmath_rest2oai/run_get_all_de_software.py
+++ src/zbmath_rest2oai/run_get_all_de_software.py
@@ -5,4 +5,4 @@ from zbmath_rest2oai.writeOai import write_oai
 #if __name__ == '__main__':
     #get_all_de('https://api.zbmath.org/v1/software/_search?search_string=si%3A',CSV_URL, 'oai:swmath.org:')
 write_oai(api_source='https://api.zbmath.org/v1/software/_search?search_string=si%3A0-100&page=0&results_per_page=100', prefix='oai:swmath.org:')
-    #get_all_de('https://api.zbmath.org/v1/software/_search?search_string=si%3A100-200&page=0&results_per_page=100', 'oai:swmath.org:')
+write_oai(api_source='https://api.zbmath.org/v1/software/_search?search_string=si%3A100-200&page=0&results_per_page=100', prefix='oai:swmath.org:')
