d1b80de170d833b81b453297693e989523c7a5c5
shirazos7 <shiraz@zbmath.org>
2024-03-25 16:29:14 +0100

    adding an if statment to the code to ensure that the output of the xml is ensuring and matching in input of our xslt , regarding to the stylesheet section ( Especially the xsi:schemaLocation )

--- test/test_metadata_article_Datacite.py
+++ test/test_metadata_article_Datacite.py
@@ -1,27 +1,42 @@
 import unittest
-
+import sys
+print(sys.path)
+import os
 import lxml.etree as ET
 from xmldiff import main
 from xmldiff.actions import MoveNode
-# should i use the same imports
 
+os.chdir(r"C:\Users\smm\PycharmProjects\python-zbMathRest2Oai")
 class PlainXmlTest(unittest.TestCase):
     def test_similarity(self):
-        dom = ET.parse('test/data/software/plain.xml')  # wthat is dom
-        xslt = ET.parse('xslt/articles/xslt-article-Datacite.xslt')
+
+        print(os.getcwd())
+        dom = ET.parse(r'test\data\articles\plain.xml')  #wthat is dom  plain.xml
+        print("input XML Document" , dom)
+        xslt = ET.parse(r'xslt\articles\xslt-article-Datacite.xslt')  #Datacite xslt
+        print("here is xslt " , xslt)
         transform = ET.XSLT(xslt) # is it a reserved word
         newdom = transform(dom)
+        if newdom.getroot().attrib.get('{http://www.w3.org/2001/XMLSchema-instance}schemaLocation') is None:
+            newdom.getroot().attrib[
+                '{http://www.w3.org/2001/XMLSchema-instance}schemaLocation'] = "http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4/metadata.xsd"
+
         real_string = ET.tostring(newdom, pretty_print=True, encoding='utf8').decode()
+        print("Transformed XML:", real_string)
         # print(ET.tostring(newdom, pretty_print=True, xml_declaration=True, encoding='utf8').decode())
+        expected_dom = ET.parse(r'test\data\articles\Test_Reference.xml')
 
-        expected_string = ET.tostring(ET.parse('test/data/software/reference.xml')) # still needs a tiny work
-
+        expected_string = ET.tostring(ET.parse(r'test\data\articles\Test_Reference.xml')) #my Test_Reference  still needs a tiny work
+        print("Expected XML:", expected_string)
         diff = main.diff_texts(expected_string, real_string, {
             'ratio_mode': 'fast', # is that for latency
             'F': 1,
         })
         essentials = list(filter(lambda e: not isinstance(e, MoveNode), diff))
-        self.assertLessEqual(len(essentials), 0)  # and actually for the equality here
+        print("Essential differences: = " , essentials)
+
+        self.assertLessEqual(len(essentials), 0 , "Found differences between expected and transformed XML")  #and actually for the equality here
+
 
 
 if __name__ == '__main__':
