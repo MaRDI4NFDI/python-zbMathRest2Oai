86cc3baffa36ffee1448e2128911d40797d9aedd
shirazos7 <shiraz@zbmath.org>
2024-05-02 17:28:43 +0200

    the current code

--- src/zbmath_rest2oai/run_get_all_de_software.py
+++ src/zbmath_rest2oai/run_get_all_de_software.py
@@ -3,7 +3,7 @@ api_source = 'https://api.zbmath.org/v1/software/_search?search_string=si%3A0-10
 prefix = 'oai:swmath.org:'
 
 
-auth = ('swmath', '3Lye4iH5mdXcjDQipN9g')
+
 
 #CSV_URL = 'https://raw.githubusercontent.com/MaRDI4NFDI/python-zbMathRest2Oai/main/src/zbmath_rest2oai/software.csv'
 #if __name__ == '__main__':
