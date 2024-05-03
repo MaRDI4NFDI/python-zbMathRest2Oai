92b8ef8d15a2172a6ba35b0ea4c562830e41b295
Mazztok45 <maxence.azzouz@gmail.com>
2024-05-03 14:42:07 +0200

    modifying the endpoint to write local software

--- src/zbmath_rest2oai/run_write_local_all_software.py
+++ src/zbmath_rest2oai/run_write_local_all_software.py
@@ -4,4 +4,4 @@ sys.path.append(os.getcwd())
 from writeLocal import write_local
 
 
-write_local(api_source='https://api.zbmath.org/v1/software/_search?search_string=si%3A0-100&page=0&results_per_page=100')
+write_local(api_source='https://api.zbmath.org/v1/software/_all?start_after=0&results_per_request=500')
