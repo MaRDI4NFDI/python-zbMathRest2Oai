2a148a9c4d129cf042af2e77dea0d6c0034a6b82
Mazztok45 <maxence.azzouz@gmail.com>
2024-05-03 13:03:13 +0200

    adding the endpoint to get all software

--- src/zbmath_rest2oai/run_get_all_de_software.py
+++ src/zbmath_rest2oai/run_get_all_de_software.py
@@ -4,4 +4,5 @@ from zbmath_rest2oai.writeOai import write_oai
 #CSV_URL = 'https://raw.githubusercontent.com/MaRDI4NFDI/python-zbMathRest2Oai/main/src/zbmath_rest2oai/software.csv'
 #if __name__ == '__main__':
     #get_all_de('https://api.zbmath.org/v1/software/_search?search_string=si%3A',CSV_URL, 'oai:swmath.org:')
-write_oai(api_source='https://api.zbmath.org/v1/software/_search?search_string=si%3A0-100&page=0&results_per_page=100', prefix='oai:swmath.org:')
+write_oai(api_source='https://api.zbmath.org/v1/software/_all?start_after=0&results_per_request=500', prefix='oai:swmath.org:')
+
