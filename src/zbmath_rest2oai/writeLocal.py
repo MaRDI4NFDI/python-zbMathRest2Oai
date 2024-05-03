a1e3703b8e9e3c66fc4913430594d6f1144a2eee
Mazztok45 <maxence.azzouz@gmail.com>
2024-05-03 17:18:00 +0200

    modifying writeLocal.py

--- src/zbmath_rest2oai/writeLocal.py
+++ src/zbmath_rest2oai/writeLocal.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 sys.path.append(os.getcwd())
 
 from zbmath_rest2oai import getAsXml
@@ -8,8 +9,8 @@ from zbmath_rest2oai import getAsXml
 def write_local(api_source):
     xml = getAsXml.final_xml2(api_source)
     filename = api_source.split('start_after=')[1].split('&')[0]
-    with open('temp_folder_software_metadata/{}.xml'.format(filename), "wb") as f:
-        f.write(xml)
+    with open('temp_folder_software_metadata/{}.xml'.format(filename), "w") as f:
+        json.dump(xml,f)
         f.close()
 
 if __name__ == '__main__':
