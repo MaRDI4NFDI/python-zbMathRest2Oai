4cbf6822c64ec8b5ae72803966295fe48b45fd60
Mazztok45 <maxence.azzouz@gmail.com>
2024-05-08 16:47:45 +0200

    using the same loop to write on the oai server than for locally writing data

--- src/zbmath_rest2oai/run_get_all_de_software.py
+++ src/zbmath_rest2oai/run_get_all_de_software.py
@@ -1,8 +1,25 @@
 from zbmath_rest2oai.writeOai import write_oai
 
 
+### OLD CODE
+
 #CSV_URL = 'https://raw.githubusercontent.com/MaRDI4NFDI/python-zbMathRest2Oai/main/src/zbmath_rest2oai/software.csv'
 #if __name__ == '__main__':
     #get_all_de('https://api.zbmath.org/v1/software/_search?search_string=si%3A',CSV_URL, 'oai:swmath.org:')
-write_oai(api_source='https://api.zbmath.org/v1/software/_all?start_after=0&results_per_request=500', prefix='oai:swmath.org:')
+#write_oai(api_source='https://api.zbmath.org/v1/software/_all?start_after=0&results_per_request=500', prefix='oai:swmath.org:')
 
+###
+
+
+with open('last_id.txt','r') as f:
+    last_id = max(f.read().split(';'))
+    f.close()
+
+
+while int(last_id) < 70000:
+    with open('last_id.txt', 'r') as f:
+        last_id = str(max([int(x) for x in f.read().split(';')]))
+        f.close()
+        uri_request = 'https://api.zbmath.org/v1/software/_all?start_after={0}&results_per_request=500'.format(last_id)
+        print(uri_request)
+        write_oai(api_source=uri_request, prefix='oai:swmath.org:')
