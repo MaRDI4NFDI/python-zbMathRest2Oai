720ac9805d9a7ccb7198c568aa994dfabcd34251
Mazztok45 <maxence.azzouz@gmail.com>
2024-05-08 17:41:18 +0200

    changes to push software metadata files to the datacite format on the OAI server

--- src/zbmath_rest2oai/writeOai.py
+++ src/zbmath_rest2oai/writeOai.py
@@ -11,7 +11,12 @@ from zbmath_rest2oai import getAsXml
 def write_oai(api_source, prefix):
     test_xml = getAsXml.final_xml2(api_source)
     for identifier in test_xml.keys():
-        files = {"item": (None, json.dumps({"identifier": prefix+str(identifier),"deleteFlag": False,"ingestFormat": "zbmath_rest_api",}),"application/json",),"content": (None, test_xml[identifier]),}
+        ingestFormat = "zbmath_rest_api"
+        if prefix.startswith('oai:swmath.org:'):
+            ingestFormat = 'datacite_swmath'
+            print(ingestFormat)
+            print(prefix+str(identifier))
+        files = {"item": (None, json.dumps({"identifier": prefix+str(identifier),"deleteFlag": False,"ingestFormat": ingestFormat,}),"application/json",),"content": (None, test_xml[identifier]),}
         basic = HTTPBasicAuth('swmath', os.environ.get('OAI_BASIC_PASSWORD'))
         x = requests.post("http://oai-input.portal.mardi4nfdi.de/oai-backend/item", files=files, auth=basic)
         if x.status_code == 409:
