488d1fe278cff5bdd3c20a68ff5540ce02cdfc09
Azzouz-Thuderoz <maxence.azzouz@gmail,com>
2024-05-11 12:37:06 +0200

    deletion of old files + modifying code to write local

--- src/zbmath_rest2oai/writeLocal.py
+++ src/zbmath_rest2oai/writeLocal.py
@@ -3,14 +3,14 @@ import os
 import json
 sys.path.append(os.getcwd())
 
-from zbmath_rest2oai import getAsXml
+from zbmath_rest2oai import getAsJSON
 
 
 def write_local(api_source):
-    xml = getAsXml.final_xml2(api_source)
+    json = getAsJSON.final_json(api_source)
     filename = api_source.split('start_after=')[1].split('&')[0]
     with open('temp_folder_software_metadata/{}.xml'.format(filename), "w") as f:
-        json.dump(xml,f)
+        json.dump(json,f)
         f.close()
 
 if __name__ == '__main__':
