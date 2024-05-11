034a0345b33a032be2fb50bbb5d7bdda2342bd91
Azzouz-Thuderoz <maxence.azzouz@gmail,com>
2024-05-11 12:51:07 +0200

    renaming json object that was overwriting the package + changing extension to json file

--- src/zbmath_rest2oai/writeLocal.py
+++ src/zbmath_rest2oai/writeLocal.py
@@ -7,10 +7,10 @@ from zbmath_rest2oai import getAsJSON
 
 
 def write_local(api_source):
-    json = getAsJSON.final_json(api_source)
+    json_res = getAsJSON.final_json(api_source)
     filename = api_source.split('start_after=')[1].split('&')[0]
-    with open('temp_folder_software_metadata/{}.xml'.format(filename), "w") as f:
-        json.dump(json,f)
+    with open('temp_folder_software_metadata/{}.json'.format(filename), "w") as f:
+        json.dump(json_res,f)
         f.close()
 
 if __name__ == '__main__':
