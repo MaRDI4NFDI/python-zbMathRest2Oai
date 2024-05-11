8516ec918de6bcf3a3e24428fa40fc6dbaa28e24
Azzouz-Thuderoz <maxence.azzouz@gmail,com>
2024-05-11 13:56:29 +0200

    adding code to delete olf files

--- src/zbmath_rest2oai/writeLocal.sh
+++ src/zbmath_rest2oai/writeLocal.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 #mkdir temp_folder_software_metadata
+
+
+if [ -f last_id.txt ]; then
+  echo "last_id.txt exists"
+  rm last_id.txt
+  echo "last_id.txt deleted"
+fi
+if [ -r temp_folder_software_metadata ]; then
+  echo "temp_folder_software_metadata exists"
+  rm -r temp_folder_software_metadata
+  echo "temp_folder_software_metadata deleted"
+fi
 echo 0 > last_id.txt
 mkdir temp_folder_software_metadata
 python3 run_write_local_all_software.py
