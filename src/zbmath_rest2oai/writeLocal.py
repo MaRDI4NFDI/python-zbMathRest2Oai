c0545122619bcf8f8766e156720cd2d30fb0c246
shirazos7 <shiraz@zbmath.org>
2024-05-23 20:39:41 +0200

    adding the file writeLocal.py in the progress to complete fthe files

---
+++ src/zbmath_rest2oai/writeLocal.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import json
+sys.path.append(os.getcwd())
+
+from zbmath_rest2oai import getAsJSON
+
+
+def write_local(api_source):
+    json_res = getAsJSON.final_json(api_source)
+    filename = api_source.split('start_after=')[1].split('&')[0]
+    with open('temp_folder_software_metadata/{}.json'.format(filename), "w") as f:
+        json.dump(json_res,f)
+        f.close()
+
+if __name__ == '__main__':
+    import sys
+    write_local(sys.argv[1])
