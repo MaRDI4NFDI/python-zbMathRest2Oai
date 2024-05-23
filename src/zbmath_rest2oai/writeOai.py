9febd36018fd5c007d0923923f1bd69d228e9d9f
shirazos7 <shiraz@zbmath.org>
2024-05-23 20:25:16 +0200

    replacing the code in the writeOai.py to make a new implementation for the articles on the oai server

--- src/zbmath_rest2oai/writeOai.py
+++ src/zbmath_rest2oai/writeOai.py
@@ -1,27 +1,24 @@
 import json
+
 import requests
 import os
 
 from requests.auth import HTTPBasicAuth
+
 from zbmath_rest2oai import getAsXml
 
-def write_oai(api_source, prefix, auth):
+
+def write_oai(api_source, prefix):
     test_xml = getAsXml.final_xml2(api_source)
     for identifier in test_xml.keys():
-        files = {
-            "item": (
-                None,
-                json.dumps({
-                    "identifier": prefix + str(identifier),
-                    "deleteFlag": False,
-                    "ingestFormat": "zbmath_rest_api",
-                }),
-                "application/json",
-            ),
-            "content": (None, test_xml[identifier]),
-        }
-        print(auth)
-        x = requests.post("http://oai-input.portal.mardi4nfdi.de/oai-backend/item", files=files, auth=auth)
+        ingestFormat = "zbmath_rest_api"
+        if prefix.startswith('oai:swmath.org:'):
+            ingestFormat = 'datacite_swmath'
+            print(ingestFormat)
+            print(prefix+str(identifier))
+        files = {"item": (None, json.dumps({"identifier": prefix+str(identifier),"deleteFlag": False,"ingestFormat": ingestFormat,}),"application/json",),"content": (None, test_xml[identifier]),}
+        basic = HTTPBasicAuth('swmath', os.environ.get('OAI_BASIC_PASSWORD'))
+        x = requests.post("http://oai-input.portal.mardi4nfdi.de/oai-backend/item", files=files, auth=basic)
         if x.status_code == 409:
             continue
         elif x.status_code != 200:
@@ -29,14 +26,8 @@ def write_oai(api_source, prefix, auth):
         else:
             return x.text
 
+
 if __name__ == '__main__':
     import sys
-    print("Number of arguments:", len(sys.argv))
-    print("Argument List:", str(sys.argv))
-    if len(sys.argv) != 3:
-        print("Usage: python writeOai.py <api_source> <prefix>")
-        sys.exit(1)
-    api_source = sys.argv[1]
-    prefix = sys.argv[2]
-
+    write_oai(sys.argv[1], sys.argv[2])
 
