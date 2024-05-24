50faf9bcb12d4c41918c7f8fd1db421f9aacce41
shirazos7 <shiraz@zbmath.org>
2024-05-24 13:41:03 +0200

    replacing the code to run it for the documents

--- src/zbmath_rest2oai/run_get_all_de_documents.py
+++ src/zbmath_rest2oai/run_get_all_de_documents.py
@@ -1,6 +1,15 @@
-from zbmath_rest2oai.get_all_de import get_all_de
+from zbmath_rest2oai.writeOai import write_oai
 
-CSV_URL = 'https://github.com/MaRDI4NFDI/python-zbMathRest2Oai/releases/download/test/all_de_240115.csv'
 
-if __name__ == '__main__':
-    get_all_de('https://api.zbmath.org/v1/document/',CSV_URL, 'oai:zbmath.org:')
+with open('last_id.txt','r') as f:
+    last_id = max(f.read().split(';'))
+    f.close()
+
+
+while int(last_id) < 70000:
+    with open('last_id.txt', 'r') as f:
+        last_id = str(max([int(x) for x in f.read().split(';')]))
+        f.close()
+        uri_request = 'https://api.zbmath.org/v1/document/_all?start_after=0&results_per_request=500'.format(last_id)
+        print(uri_request)
+        write_oai(api_source=uri_request, prefix='oai:swmath.org:')
