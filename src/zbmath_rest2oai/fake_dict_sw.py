47d9ffdf0b64ff7f4f1a7e5b37de16bf2cfd6c9e
Mazztok45 <maxence.azzouz@gmail.com>
2024-05-30 17:06:10 +0200

    deleting software.csv

--- src/zbmath_rest2oai/fake_dict_sw.py
+++
@@ -1,6 +0,0 @@
-def fake_dict_sw():
-    with open('software.csv','w') as f:
-        return f.write("\n".join(map(str,list(range(2, 10)))))
-
-if __name__ == '__main__':
-    fake_dict_sw()
--- src/zbmath_rest2oai/writeOai.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#mkdir temp_folder_software_metadata
-echo 0 > last_id.txt
-python3 run_get_all_de_software.py
-rm last_id.txt
