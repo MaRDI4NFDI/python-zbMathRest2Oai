344b893f330e68f4679ffc815a351fc1bba82661
Mazztok45 <maxence.azzouz@gmail.com>
2024-06-07 16:01:00 +0200

    adding code to delete item, do not clearly work

--- formats/cleaningfiles.sh
+++ formats/cleaningfiles.sh
@@ -3,4 +3,5 @@ AUTH=$(echo -n "$OAI_BASIC_USER:$OAI_BASIC_PASSWORD" | base64)
 # This is a test to delete crosswalks. It is working, I could delete the following one:
 #curl --noproxy '*' -X DELETE -H  "Authorization: Basic $AUTH" -i 'https://oai-input.portal.mardi4nfdi.de/oai-backend/crosswalk/swmath2datacite2'
 
-
+# Strange result. This test could change the deleteFlag from False to True. Don't know yet if the item will later disappear:
+curl --noproxy '*' -X DELETE -H  "Authorization: Basic $AUTH" -i "https://oai-input.portal.mardi4nfdi.de/oai-backend/item/6477"
