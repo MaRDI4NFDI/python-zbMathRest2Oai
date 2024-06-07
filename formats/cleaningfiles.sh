f30d4ecddfc70b402cb77d7cbe86f0363ddc91ad
Mazztok45 <maxence.azzouz@gmail.com>
2024-06-07 15:28:06 +0200

    can delete crosswalks

--- formats/cleaningfiles.sh
+++ formats/cleaningfiles.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+AUTH=$(echo -n "$OAI_BASIC_USER:$OAI_BASIC_PASSWORD" | base64)
+# This is a test to delete crosswalks. It is working, I could delete the following one:
+#curl --noproxy '*' -X DELETE -H  "Authorization: Basic $AUTH" -i 'https://oai-input.portal.mardi4nfdi.de/oai-backend/crosswalk/swmath2datacite2'
+
+
