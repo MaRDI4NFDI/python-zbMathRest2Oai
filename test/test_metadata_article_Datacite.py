5718e2bc8d0164a1a9053ec01051b90f0de42cae
physikerwelt <wiki@physikerwelt.de>
2024-06-21 12:12:12 +0200

    Remove print statements
    Print statements clutter the output and make it hard to debug tests

--- test/test_metadata_article_Datacite.py
+++ test/test_metadata_article_Datacite.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+
 print(sys.path)
 import os
 import lxml.etree as ET
@@ -7,8 +8,10 @@ import re
 from xmldiff import main
 from xmldiff.actions import MoveNode
 
-if os.path.basename(os.getcwd())=='test':
+if os.path.basename(os.getcwd()) == 'test':
     os.chdir(os.path.dirname(os.getcwd()))
+
+
 # Function to split DOI into parts
 def split_doi(doi):
     # Define regular expression patterns for various DOI formats
@@ -16,7 +19,6 @@ def split_doi(doi):
         r"(10\.\d{4,}/\S+)",
         r"(/?\S+)",  # For DOIs without prefix
     ]
-
 
     # Iterate through patterns to find a match
     for pattern in doi_patterns:
@@ -30,9 +32,7 @@ class PlainXmlTest(unittest.TestCase):
     def test_similarity(self):
 
         dom = ET.parse('test/data/articles/plain.xml')  #wthat is dom  plain.xml
-        print("input XML Document" , dom)
         xslt = ET.parse('xslt/articles/xslt-article-Datacite.xslt')  #Datacite xslt
-        print("here is xslt " , xslt)
 
         # Preprocess DOI values before transformation
         for elem in dom.xpath("//references/doi"):
@@ -43,31 +43,25 @@ class PlainXmlTest(unittest.TestCase):
                     elem.text = f"{doi_prefix}/"
                     if doi_suffix:
                         elem.attrib["suffix"] = doi_suffix
-        transform = ET.XSLT(xslt) # is it a reserved word
-        print(transform)
+        transform = ET.XSLT(xslt)  # is it a reserved word
         newdom = transform(dom)
-
 
         if newdom.getroot().attrib.get('{http://www.w3.org/2001/XMLSchema-instance}schemaLocation') is None:
             newdom.getroot().attrib[
                 '{http://www.w3.org/2001/XMLSchema-instance}schemaLocation'] = "http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4/metadata.xsd"
 
         real_string = ET.tostring(newdom, pretty_print=True, encoding='utf8').decode()
-        print("Transformed XML:", real_string)
-        # print(ET.tostring(newdom, pretty_print=True, xml_declaration=True, encoding='utf8').decode())
-        expected_dom = ET.parse('test/data/articles/Test_Reference.xml')
+        # test if result is parsable
+        ET.parse('test/data/articles/Test_Reference.xml')
 
-        expected_string = ET.tostring(ET.parse('test/data/articles/Test_Reference.xml')) #my Test_Reference  still needs a tiny work
-        print("\nExpected XML:", expected_string)
+        expected_string = ET.tostring(ET.parse('test/data/articles/Test_Reference.xml'))
         diff = main.diff_texts(expected_string, real_string, {
-            'ratio_mode': 'fast', # is that for latency
+            'ratio_mode': 'fast',  # is that for latency
             'F': 1,
         })
         essentials = list(filter(lambda e: not isinstance(e, MoveNode), diff))
-        print("\nEssential differences: = " , essentials)
 
-        self.assertLessEqual(len(essentials), 0 , "Found differences between expected and transformed XML")  #and actually for the equality here
-
+        self.assertLessEqual(len(essentials), 0, "Found differences between expected and transformed XML")
 
 
 if __name__ == '__main__':
