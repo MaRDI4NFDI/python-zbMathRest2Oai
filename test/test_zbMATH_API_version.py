b8960d8861437f6c95e24407f65d74acdb71357f
physikerwelt <wiki@physikerwelt.de>
2024-06-24 20:22:53 +0200

    Enable aoi version test
    Close #45

--- test/test_zbMATH_API_version.py
+++ test/test_zbMATH_API_version.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
 
+
 def get_api_version(url):
     try:
         response = requests.get(url)
@@ -11,14 +12,15 @@ def get_api_version(url):
     except requests.RequestException as e:
         return f"Error fetching OpenAPI data: {e}"
 
+
 class TestAPIVersion(unittest.TestCase):
     def test_api_version_matches(self):
-        # Example URL - replace this with the actual URL you're testing
-        url = "https://api.zbmath.org/openapi.json" # the URL of our openapi-zbmath
-        expected_version = "1.4.3"
+        url = "https://api.zbmath.org/openapi.json"
+        expected_version = "1.5.1"
         actual_version = get_api_version(url)
-        self.assertEqual(actual_version, expected_version, f"Expected API version {expected_version}, but got {actual_version}")
-        print("The Expected Version is " + expected_version) # to see if its matching the version's number
+        self.assertEqual(actual_version, expected_version,
+                         f"Expected API version {expected_version}, but got {actual_version}")
+
+
 if __name__ == "__main__":
     unittest.main()
-
