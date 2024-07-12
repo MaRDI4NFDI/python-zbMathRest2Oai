7b52e0238559e66116853d0f21a7a51935258a12
physikerwelt <wiki@physikerwelt.de>
2024-07-12 17:43:26 +0200

    Remove invalid characters from XML
    Adapted from 32f14cc18dbed6a57432c686fc7b5b4e8359983f in
    the previous oai-pmh implementation.

--- src/zbmath_rest2oai/getAsXml.py
+++ src/zbmath_rest2oai/getAsXml.py
@@ -1,6 +1,41 @@
+import re
+
 from dict2xml import Converter
 import requests
 import sys
+from urllib3.packages.six import unichr
+
+# from https://stackoverflow.com/a/22273639
+
+_illegal_unichrs = [(0x00, 0x08), (0x0B, 0x0C), (0x0E, 0x1F),
+
+                    (0x7F, 0x84), (0x86, 0x9F),
+
+                    (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF)]
+
+if sys.maxunicode >= 0x10000:  # not narrow build
+
+    _illegal_unichrs.extend([(0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF),
+
+                             (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF),
+
+                             (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
+
+                             (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF),
+
+                             (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF),
+
+                             (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
+
+                             (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF),
+
+                             (0xFFFFE, 0xFFFFF), (0x10FFFE, 0x10FFFF)])
+
+_illegal_ranges = ["%s-%s" % (unichr(low), unichr(high))
+
+                   for (low, high) in _illegal_unichrs]
+
+_illegal_xml_chars_RE = re.compile(u'[%s]' % u''.join(_illegal_ranges))
 
 
 def apply_zbmath_api_fixes(result, prefix):
@@ -29,9 +64,9 @@ def final_xml2(api_source, prefix):
     dict_math_entities = dict()
     for result in json["result"]:
         apply_zbmath_api_fixes(result, prefix)
-        dict_math_entities[result["id"]] = Converter(wrap="root").build(
+        dict_math_entities[result["id"]] = _illegal_xml_chars_RE.sub("", Converter(wrap="root").build(
             result,
-            closed_tags_for=[[], '', [None], None])
+            closed_tags_for=[[], '', [None], None]))
     return [dict_math_entities, r.elapsed.total_seconds()]
 
 
---
+++ test/test_invalid_char_xml.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+
+from xmldiff import main
+from xmldiff.actions import MoveNode
+
+from zbmath_rest2oai import getAsXml
+
+API_SOURCE = 'https://api.zbmath.org/v1/document/_structured_search?page=0&results_per_page=10&zbmath%20id=6389213'
+
+
+class PlainXmlTest(unittest.TestCase):
+    def test_similarity(self):
+        real_string = getAsXml.final_xml2(API_SOURCE, 'Zbl ')[0]['Zbl 6389213']
+
+        ref_location = os.path.join(os.path.dirname(__file__), 'data/articles/invalid_chr.xml')
+        with open(ref_location) as f:
+            expected_string = f.read()
+            diff = main.diff_texts(expected_string, real_string, {
+                'ratio_mode': 'fast',
+                'F': 1,
+            })
+            essentials = list(filter(lambda e: not isinstance(e, MoveNode), diff))
+            self.assertLessEqual(len(essentials), 1)  # TODO: find a way to store trailing spaces
+
+
+if __name__ == '__main__':
+    unittest.main()
