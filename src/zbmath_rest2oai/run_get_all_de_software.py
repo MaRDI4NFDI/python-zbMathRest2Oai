e8aa9031d8ed678876e34b00fe66e115d43b8775
physikerwelt <wiki@physikerwelt.de>
2024-10-14 21:09:37 +0200

    Use RFC 2396 compatible identifiers
    Close #100

--- src/zbmath_rest2oai/run_get_all_de_software.py
+++ src/zbmath_rest2oai/run_get_all_de_software.py
@@ -1,7 +1,7 @@
 from zbmath_rest2oai.get_all import get_all
 
 get_all(
-        prefix='swmath',
+        prefix='oai:swmath.org:',
         url='https://api.zbmath.org/v1/software/_all?start_after={0}&results_per_request=500',
         ingest_format='zbmath_rest_api_software',
         state_property='software'
