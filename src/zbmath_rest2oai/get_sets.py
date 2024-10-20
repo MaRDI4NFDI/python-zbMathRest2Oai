e2e2ecbfd4c076c967c44c77cc479d90c4bda7c8
physikerwelt <wiki@physikerwelt.de>
2024-10-20 22:48:42 +0200

    Add script to get sets

---
+++ src/zbmath_rest2oai/get_sets.py
@@ -0,0 +1,20 @@
+import requests
+
+URL = 'https://api.zbmath.org/v1/classification/_search?search_string=lv%3A0'
+
+
+def get_sets():
+    headers = {'Accept': 'application/json'}
+    r = requests.get(URL, headers=headers)
+    results = r.json()['result']
+    mscs = {'JFM': 'JFM: Jahrbuch für Mathematik',
+            'openaire': 'openaire Required set for OpenAIRE, cf.'
+                        + 'https://guidelines.openaire.eu/en/latest/literature'
+                        + '/use_of_oai_pmh.html'}
+    for result in results:
+        mscs[result['code'][:2]] = result['short_title']
+    return mscs
+
+
+if __name__ == '__main__':
+    print(get_sets())
