d9d83be2e4c850064789d1087f18a870bb031845
Mazztok45 <maxence.azzouz@gmail.com>
2024-11-06 17:01:29 +0100

    adjusting in staging_deposit.py to import the code without error + using the SWHID dir and not anymore the SWHID snapshot

--- src/staging_deposit.py
+++ src/staging_deposit.py
@@ -1,5 +1,17 @@
+from zbmath_rest2oai.restApi_software_Json import process_metadata
+from zbmath_rest2oai.convertSoftware_from_json_toXml import convert_json_to_xml
+import lxml
+import requests
 
 
-restApi_software_Json.py
-convertSoftware_from_json_toXml.py
-xslt xslt-software-Codemeta.xslt
+#csv_file_path = '../test/data/software/swh_swmath.csv'  # Path to your CSV file
+csv_file_path = '../test/data/software/swh_swmath_swhid_dir.csv'  # Path to your CSV file
+api_url = 'https://api.zbmath.org/v1/software/825'  # API URL, can be generalized for any software ID
+json_output_path = '../test/data/software/software_with_swhid.json'
+xml_output_path = '../test/data/software/software_with_swhid.xml'  # Update with your desired output path
+output_log_filename = '../test/data/software/logfile.txt'
+
+
+process_metadata(output_log_filename, csv_file_path, api_url, json_output_path)
+
+convert_json_to_xml(json_output_path, xml_output_path)
