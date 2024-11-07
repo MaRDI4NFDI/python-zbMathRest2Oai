bc53bd2e4f839e81848dd18c310e49dff11c9bed
physikerwelt <wiki@physikerwelt.de>
2024-11-07 22:48:30 +0100

    Make statefile configurable

--- src/zbmath_rest2oai/get_all.py
+++ src/zbmath_rest2oai/get_all.py
@@ -1,10 +1,12 @@
+import os
+
 from zbmath_rest2oai.getAsXml import EntryNotFoundException
 from zbmath_rest2oai.state import State
 from zbmath_rest2oai.writeOai import write_oai
 
 
 def get_all(prefix, url, ingest_format, state_property):
-    state = State()
+    state = State(os.environ.get('OAI_STATE_PATH', 'state.json'))
     while True:
         uri_request = url.format(state.get_state_var(state_property))
         try:
