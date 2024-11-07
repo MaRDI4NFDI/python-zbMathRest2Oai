347d671b0431645617fcc77da9216848a549f9ec
physikerwelt <wiki@physikerwelt.de>
2024-11-07 16:43:52 +0100

    Only write changes
    2 Requests are being issued
    
    1) get current content
    
    if 404
    
    old behaviour
    
    if 200
    
    compare content
    
    if different put new content
    
    otherwise do nothing

--- src/zbmath_rest2oai/writeOai.py
+++ src/zbmath_rest2oai/writeOai.py
@@ -13,7 +13,34 @@ AUTH = aiohttp.BasicAuth('swmath', os.environ.get('OAI_BASIC_PASSWORD'))
 URL = "https://oai-input.portal.mardi4nfdi.de/oai-backend/item"
 
 
+def content_differs(current, content) -> bool:
+    return content != current['content']['content']
+
+
 async def sync_item(session, identifier, tags, ingest_format, content):
+    async with session.get(URL + '/' + str(identifier) + '?content=true', auth=AUTH) as resp:
+        if resp.status == 404:
+            data = await format_data(content, identifier, ingest_format, tags)
+            async with session.post(URL, data=data, auth=AUTH) as response:
+                if response.status not in [200]:
+                    raise Exception(f"Unexpected response with status code {response.status} for item {identifier}:"
+                                    f" {await response.text()}")
+                return response
+        if resp.status == 200:
+            current = await resp.json()
+            if content_differs(current, content):
+                data = await format_data(content, identifier, ingest_format, tags)
+                async with session.put(URL + '/' + str(identifier), data=data, auth=AUTH) as response:
+                    if response.status not in [200]:
+                        raise Exception(f"Unexpected response with status code {response.status} for item {identifier}:"
+                                        f" {await response.text()}")
+                    return response
+            else:
+                # nothing was changed
+                pass
+
+
+async def format_data(content, identifier, ingest_format, tags):
     data = aiohttp.FormData()
     data.add_field('item', json.dumps({
         "identifier": str(identifier),
@@ -22,11 +49,7 @@ async def sync_item(session, identifier, tags, ingest_format, content):
         "ingestFormat": ingest_format
     }), content_type='application/json')
     data.add_field('content', content, filename='dummy.xml')
-    async with session.post(URL, data=data, auth=AUTH) as response:
-        if response.status not in [200, 409]:
-            raise Exception(f"Unexpected response with status code {response.status} for item {identifier}:"
-                            f" {await response.text()}")
-        return response
+    return data
 
 
 async def async_write_oai(xml_contents, ingest_format, tags=None):
