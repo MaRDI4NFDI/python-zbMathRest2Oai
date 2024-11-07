467fff6df388c0a0e83ea69c3bd3de54e5949989
physikerwelt <wiki@physikerwelt.de>
2024-11-07 15:38:10 +0100

    refactor writeOai
    * prepare for more complex update logic

--- src/zbmath_rest2oai/writeOai.py
+++ src/zbmath_rest2oai/writeOai.py
@@ -13,8 +13,16 @@ AUTH = aiohttp.BasicAuth('swmath', os.environ.get('OAI_BASIC_PASSWORD'))
 URL = "https://oai-input.portal.mardi4nfdi.de/oai-backend/item"
 
 
-async def post_item(session, files, identifier='unknown'):
-    async with session.post(URL, data=files, auth=AUTH) as response:
+async def sync_item(session, identifier, tags, ingest_format, content):
+    data = aiohttp.FormData()
+    data.add_field('item', json.dumps({
+        "identifier": str(identifier),
+        "deleteFlag": False,
+        "tags": tags,
+        "ingestFormat": ingest_format
+    }), content_type='application/json')
+    data.add_field('content', content, filename='dummy.xml')
+    async with session.post(URL, data=data, auth=AUTH) as response:
         if response.status not in [200, 409]:
             raise Exception(f"Unexpected response with status code {response.status} for item {identifier}:"
                             f" {await response.text()}")
@@ -32,15 +40,12 @@ async def async_write_oai(xml_contents, ingest_format, tags=None):
         for identifier in xml_contents.keys():
             records += 1
             last_id = identifier
-            data = aiohttp.FormData()
-            data.add_field('item', json.dumps({
-                "identifier": str(identifier),
-                "deleteFlag": False,
-                "tags": tags.get(identifier, []),
-                "ingestFormat": ingest_format
-            }), content_type='application/json')
-            data.add_field('content', xml_contents[identifier], filename='dummy.xml')
-            tasks.append(post_item(session, data, identifier))
+            tasks.append(sync_item(
+                session,
+                identifier,
+                tags.get(identifier, []),
+                ingest_format,
+                xml_contents[identifier]))
 
         await asyncio.gather(*tasks)
 
