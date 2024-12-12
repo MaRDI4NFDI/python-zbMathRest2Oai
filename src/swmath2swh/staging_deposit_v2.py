64e44de679bd81bde4e8d763312b4c028aa2e7f1
shirazos7 <shiraz@zbmath.org>
2024-12-12 15:47:29 +0100

    adjusting the parser to be defusedxml and lxml for xslt transformation

--- src/swmath2swh/staging_deposit_v2.py
+++ src/swmath2swh/staging_deposit_v2.py
@@ -1,7 +1,6 @@
 from swmath2swh.restApi_software_Json import process_metadata
 from swmath2swh.convertSoftware_from_json_toXml import convert_json_to_xml
-import defusedxml.ElementTree as DET
-
+import lxml.etree as ET
 import pandas as pd
 import subprocess
 import time
@@ -9,32 +8,32 @@ import tempfile
 import os
 import requests
 import re
-
 env = os.environ.copy()
 env['SWMATH_USER_DEPOSIT'] = os.getenv('SWMATH_USER_DEPOSIT')
-env['SWMATH_PWD_DEPOSIT'] = os.getenv('SWMATH_PWD_DEPOSIT')
 env['SWMATH_PWD_DEPOSIT'] = os.getenv('SWMATH_PWD_DEPOSIT')
 xsl_filename = '../../xslt/software/xslt_SWH_deposit.xslt'
 
 r = requests.get("https://oai.staging.mardi4nfdi.org/oai/OAIHandler?verb=GetRecord&metadataPrefix=codemeta&identifier=oai:swmath.org:4532")
 xml_str = r.content
-dom = DET.fromstring(xml_str)
-formatted_newdom = DET.tostring(dom, encoding='unicode')
 
+parser = ET.XMLParser(remove_pis=True)
+dom = ET.fromstring(xml_str, parser)
 
-def replace_namespace_prefixes(xml_str, new_prefix='codemeta'):
-    updated_xml = re.sub(r'ns\d+:', f'{new_prefix}:', xml_str)
-    return updated_xml
+xslt = ET.parse(xsl_filename)
+transform = ET.XSLT(xslt)
+newdom = transform(dom)
+formatted_newdom = ET.tostring(newdom, pretty_print=True, encoding='unicode')
+formatted_newdom = re.sub(r'xmlns:ns\d+="[^"]+"', '', formatted_newdom)
+formatted_newdom = re.sub(r'ns\d+:', 'codemeta:', formatted_newdom)
+print(formatted_newdom)
 
-
-FINAL_XML = replace_namespace_prefixes(formatted_newdom, new_prefix='codemeta')
-print(FINAL_XML)
 
 with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.xml') as temp_file:
-    temp_file.write(FINAL_XML)
+    temp_file.write(formatted_newdom)
     temp_filename = temp_file.name
     print(f"Temporary file created: {temp_filename}")
-# Format current time for deposit status
+
+
 current_time = time.localtime()
 formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", current_time)
 dict_deposit = str({
@@ -43,7 +42,7 @@ dict_deposit = str({
     "deposit_date": formatted_time
 })
 
-# Run the command with the temporary XML file
+
 subprocess.run([
     "swh", "deposit", "metadata-only",
     "--username", env["SWMATH_USER_DEPOSIT"],
@@ -52,4 +51,3 @@ subprocess.run([
     "--metadata", temp_filename,
     "--format", "json"
 ])
-
