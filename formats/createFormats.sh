f4e6553b65dd61f4aef8547ce2ba87a4ff51aec1
Mazztok45 <maxence.azzouz@gmail.com>
2024-12-18 19:36:48 +0100

    switching to mardi portal for format creation

--- formats/createFormats.sh
+++ formats/createFormats.sh
@@ -4,7 +4,7 @@ AUTH=$(echo -n "$OAI_BASIC_USER:$OAI_BASIC_PASSWORD" | base64)
 
 OAI_INPUT_STAGING='https://oai-input.staging.mardi4nfdi.org/oai-backend/format'
 OAI_INPUT_PRODUCTION='https://oai-input.portal.mardi4nfdi.de/oai-backend/format'
-OAI_INPUT=$OAI_INPUT_STAGING
+OAI_INPUT=$OAI_INPUT_PRODUCTION
 
 printf "\n\nCreate Format oai_dc\n\n"
 curl --noproxy '*' -X POST -H 'Content-Type: application/json' -H  "Authorization: Basic $AUTH"  -i $OAI_INPUT --data '{"metadataPrefix":"oai_dc","schemaLocation":"http://www.openarchives.org/OAI/2.0/oai_dc.xsd","schemaNamespace":"http://www.openarchives.org/OAI/2.0/oai_dc/","identifierXpath":"/identifier"}'
