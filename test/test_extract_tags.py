653e8e747d796b8daca8d9774bd4bb96347ab91d
shirazos7 <shiraz@zbmath.org>
2025-03-28 23:46:50 +0100

    changing the tags in the unittest  code  after the  update of api version

--- test/test_extract_tags.py
+++ test/test_extract_tags.py
@@ -21,7 +21,28 @@ class PlainXmlTest(unittest.TestCase):
             'https://api.zbmath.org/v1/software/12',
             headers=headers)
         real_tags = extract_tags(r.json()['result'])
-        assert real_tags == ['60', '65', '78', '82']
+        assert real_tags == ['(optics),',
+ '2010:',
+ '60J70',
+ '65C50',
+ '78A05',
+ '78A45',
+ '82D25',
+ 'Appl.',
+ 'Crystals',
+ 'Diffraction,',
+ 'Geometric',
+ 'MSC',
+ 'Other',
+ 'computational',
+ 'diffusion',
+ 'in',
+ 'of',
+ 'optics,',
+ 'probability,',
+ 'problems',
+ 'scattering',
+ 'theory,']
 
 if __name__ == '__main__':
     unittest.main()
