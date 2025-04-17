c847a146494247446490bbefe7e2c41d9f662ce1
shirazos7 <shiraz@zbmath.org>
2025-04-17 15:09:15 +0200

    change the tags for testing purposes

--- test/test_extract_tags.py
+++ test/test_extract_tags.py
@@ -21,28 +21,7 @@ class PlainXmlTest(unittest.TestCase):
             'https://api.zbmath.org/v1/software/12',
             headers=headers)
         real_tags = extract_tags(r.json()['result'])
-        assert real_tags ==  ['(optics),',
- '2010:',
- '60J70',
- '65C50',
- '78A05',
- '78A45',
- '82D25',
- 'Appl.',
- 'Crystals',
- 'Diffraction,',
- 'Geometric',
- 'MSC',
- 'Other',
- 'computational',
- 'diffusion',
- 'in',
- 'of',
- 'optics,',
- 'probability,',
- 'problems',
- 'scattering',
- 'theory,']
+        assert real_tags == ['60', '65', '78', '82']
 
 
 
