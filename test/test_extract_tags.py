0aa466e45e7275ea750077f2af476d94cd1c80d2
Mazztok45 <maxence.azzouz@gmail.com>
2025-06-04 12:13:17 +0200

    fixing the test for tag extraction

--- test/test_extract_tags.py
+++ test/test_extract_tags.py
@@ -21,7 +21,7 @@ class PlainXmlTest(unittest.TestCase):
             'https://api.zbmath.org/v1/software/12',
             headers=headers)
         real_tags = extract_tags(r.json()['result'])
-        assert real_tags == ['60', '65', '78', '82']
+        assert real_tags == ['60', '65', '78', '82', 'openaire']
 
 
 
