b3973030aebfd1cca3ea4502c50d3fbfc68703f3
Mazztok45 <maxence.azzouz@gmail.com>
2026-02-25 14:52:50 +0100

    writing the formerly canceled term, they were relevants for the test

--- test/test_extract_tags.py
+++ test/test_extract_tags.py
@@ -12,7 +12,7 @@ class PlainXmlTest(unittest.TestCase):
             'https://api.zbmath.org/v1/document/2500495',
             headers=headers)
         real_tags = extract_tags(r.json()['result'])
-        assert real_tags == ['11', 'JFM']
+        assert real_tags == ['11', 'JFM', 'datacite']
 
     @staticmethod
     def test_software():
@@ -21,7 +21,7 @@ class PlainXmlTest(unittest.TestCase):
             'https://api.zbmath.org/v1/software/12',
             headers=headers)
         real_tags = extract_tags(r.json()['result'])
-        assert real_tags == ['60', '65', '78', '82']
+        assert real_tags == ['60', '65', '78', '82', 'openaire']
 
 
 
