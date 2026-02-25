e790b1662fdd17c74272280fc1c90cca01487ede
Mazztok45 <maxence.azzouz@gmail.com>
2026-02-25 14:14:18 +0100

    fixing invalid characters

--- test/test_invalid_char_xml.py
+++ test/test_invalid_char_xml.py
@@ -6,12 +6,12 @@ from xmldiff.actions import MoveNode
 
 from zbmath_rest2oai import getAsXml
 
-API_SOURCE = 'https://api.zbmath.org/v1/document/_structured_search?page=0&results_per_page=10&zbmath%20id=6389213'
+API_SOURCE = 'https://api.zbmath.org/v1/document/6389213'
 
 
 class PlainXmlTest(unittest.TestCase):
     def test_similarity(self):
-        real_string = getAsXml.final_xml2(API_SOURCE, 'Zbl ')[0]['Zbl 6389213']
+        real_string = getAsXml.final_xml2(API_SOURCE, 'oai:zbmath.org:')[0]['oai:zbmath.org:6389213']
 
         ref_location = os.path.join(os.path.dirname(__file__), 'data/articles/invalid_chr.xml')
         with open(ref_location) as f:
