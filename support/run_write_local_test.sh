e585afde3581a775b4842a73ad747c16b7b85c75
physikerwelt <wiki@physikerwelt.de>
2026-02-25 21:34:19 +0100

    Remove unused `run_write_local_test.sh` script

--- support/run_write_local_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python support/write_local_test.py 2 https://api.zbmath.org/v1/software/_search?search_string=si%3A /home/maxence/myvenv/python-zbMathRest2Oai/test/data/software/plain.xml
